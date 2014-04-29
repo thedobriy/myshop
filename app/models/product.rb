d0614631c5017a7fd2f36a9364da73e6426d9ac3
Dmitriy N. Chorniy <thedobriy007@gmail.com>
2014-04-29 16:50:13 +0300

    Created Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
+	has_many :line_items
 	validates :title, :description, :image_url, presence: true
 	validates :title, uniqueness: true
 	validates :price, numericality: {greater_then_or_equal_to: 0.01}
@@ -6,7 +7,19 @@ class Product < ActiveRecord::Base
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'URL должен указывать на изображение формата GIF, JPG или PNG.'
 		}
+	before_destroy :ensure_not_referenced_by_any_line_item
+
 	def self.latest
   	Product.order(:updated_at).last
 	end
+
+	private
+		def ensure_not_referenced_by_any_line_item
+			if line_items.empty?
+				return true
+			else
+				errors.add(:base, 'Существуют товарные позиции')
+				return false
+			end
+		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Myshop::Application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
   get "store/index"
   resources :products
 
