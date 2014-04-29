31f9f8af2864fa80e10e97b2160d8ff93deb6afb
Dmitriy N. Chorniy <thedobriy007@gmail.com>
2014-04-29 13:54:17 +0300

    Created css and items list

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ActiveRecord::Base
+	validates :title, :description, :image_url, presence: true
+	validates :title, uniqueness: true
+	validates :price, numericality: {greater_then_or_equal_to: 0.01}
+	validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'URL должен указывать на изображение формата GIF, JPG или PNG.'
+		}
+	def self.latest
+  	Product.order(:updated_at).last
+	end
 end
